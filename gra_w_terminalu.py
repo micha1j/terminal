29270b53a92dc53146915f144c22170a4bf94b1d
Michal <michal333878@gmail.com>
2023-08-24 19:39:49 +0200

    Dodanie klasy postaci

--- gra_w_terminalu.py
+++ gra_w_terminalu.py
@@ -1,2 +1,4 @@
-for x in range(10):
-    print("xim")
+print("Hello in game")
+num_players = input("How many people will play?(insert number like 2)")
+while num_players.isdigit() == False:
+    num_players = input("Wrong! Please insert number")
